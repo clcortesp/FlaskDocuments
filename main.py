4d34485fb7d177ee7a781750c695571f0f9d3671
Claudio Cortes <claudio.cortes@vasslatam.com>
2024-02-11 03:00:32 -0300

    main

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/app/v1/documents', methods=['GET'])
-def get_string():
-    print("test")
-    return "tu usuario"
+@app.route('/app/v1/documents', methods=['POST'])
+def receive_attachment():
+    binary_data = request.get_data()
+
+    print(binary_data)
+
+    return 'Archivo recibido con éxito.', 200
 
 app.run(debug=True)
